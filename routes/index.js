85db14b033ab67799b9bb86db8c4c69937c0993f
a1789342-jessduong <jessicaduong0812@gmail.com>
2021-04-08 10:10:50 +0000

    adding files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+
+router.get('/', function(req, res, next) {
+
+    date = new Date().toString();
+    res.send(date);
 });
 
+
+
+var date;
+
+
+router.get('/last.txt', function(req, res) {
+    res.send(date);
+    date = new Date().toString();
+});
+
+
+
 module.exports = router;
+
